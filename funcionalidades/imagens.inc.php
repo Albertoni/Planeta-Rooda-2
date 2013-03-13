a447d42a54545a8a2af91c66d8f62f8b6a8850f4
NUTED <nuted@SIDESHOWBOB.(none)>
2013-03-13 16:02:24 -0300

    Lixo removido, mockup da transparencia da luva

--- funcionalidades/imagens.inc.php
+++
@@ -1,7 +0,0 @@
-<?
-function imagem($img,$width,$height){
-	echo "<table border='0' cellspacing='0' cellpadding='0' width='$width' height='$height'><tr><td align='center' valign='center' width='$width' height='$height'>";
-	echo"<img src='/planeta/redimensiona.php?img=$img&width=$width&height=$height' border='0'/>"; 
-	echo "</td></tr></table>";
-}
-?>
