0ab93a784172f6701b7b011ba52f651dfd262b68
NUTED <nuted@SIDESHOWBOB.(none)>
2013-03-13 17:49:44 -0300

    indentação

--- menuNovo/telaInicialTurma.php
+++ menuNovo/telaInicialTurma.php
@@ -62,3 +62,52 @@
 		</div>
 	</body>
 </html>
+
+<!--
+BACKUP DO HTML ORIGINAL
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link href="menus.css" rel="stylesheet" type="text/css">
+		<script type="text/javascript" src="../jquery.js"></script>
+	</head>
+	<body>
+		<div id="containerMenu">
+			<div id="menuEsquerda">
+				<div id="infoTurma">
+					Nesse exemplo posicionamos o elemento a 40px do topo e a 
+					20px da esquerda em relação ao lugar que ele ocuparia no 
+					fluxo do documento. Vale lembrar que utilizando 
+					position:relative todo esse espaço que foi definido ainda 
+					continua sendo ocupado pelo elemento na página. 
+				</div>
+				<div id="wrapperBotoesEsquerda">
+					<div id="botaoContatos" class="botaoEsquerda"></div>
+					<div id="botaoFuncionalidade" class="botaoEsquerda"></div>
+					<div id="botaoPlaneta" class="botaoEsquerda"></div>
+				</div>
+			</div>
+			<div id="menuDireita">
+				<div id="wrapperClasses">
+					<div class="botaoDireita" id="botaoProfessores"></div>
+					<div class="botaoDireita" id="botaoMonitores"></div>
+					<div class="botaoDireita" id="botaoAlunos"></div>
+				</div>
+				<div id="listaMembrosTurma">
+					<div class="membroTurma comFundo">NOME DO PIRADO <a href="#"><img class="botaoUsuario iconeDeletar"></a><a href="#"><img class="botaoUsuario iconeCarteira"></a><a href="#"><img class="botaoUsuario iconePromocao"></a></div>
+					<div class="membroTurma">NOME DO MALUCO <a href="#"><img class="botaoUsuario iconeDeletar"></a><a href="#"><img class="botaoUsuario iconeCarteira"></a><a href="#"><img class="botaoUsuario iconePromocao"></a></div>
+					<div class="membroTurma comFundo">NOME DO PIRADO <a href="#"><img class="botaoUsuario iconeDeletar"></a><a href="#"><img class="botaoUsuario iconeCarteira"></a><a href="#"><img class="botaoUsuario iconePromocao"></a></div>
+					<div class="membroTurma">NOME DO MALUCO <a href="#"><img class="botaoUsuario iconeDeletar"></a><a href="#"><img class="botaoUsuario iconeCarteira"></a><a href="#"><img class="botaoUsuario iconePromocao"></a></div>
+					<div class="membroTurma comFundo">NOME DO PIRADO <a href="#"><img class="botaoUsuario iconeDeletar"></a><a href="#"><img class="botaoUsuario iconeCarteira"></a><a href="#"><img class="botaoUsuario iconePromocao"></a></div>
+					<div class="membroTurma">NOME DO MALUCO <a href="#"><img class="botaoUsuario iconeDeletar"></a><a href="#"><img class="botaoUsuario iconeCarteira"></a><a href="#"><img class="botaoUsuario iconePromocao"></a></div>
+					<div class="membroTurma comFundo">NOME DO PIRADO <a href="#"><img class="botaoUsuario iconeDeletar"></a><a href="#"><img class="botaoUsuario iconeCarteira"></a><a href="#"><img class="botaoUsuario iconePromocao"></a></div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
+
+ -->
