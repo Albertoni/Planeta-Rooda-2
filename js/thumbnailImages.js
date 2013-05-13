cb62b34badf85455b6d42179c5576a6828156b72
Alexandre Leuck <aleuck2004@gmail.com>
2013-05-13 14:42:11 -0300

    Auto Thumbnail javascript

--- js/thumbnailImages.js
+++ js/thumbnailImages.js
@@ -1,33 +1,37 @@
-function thumbnailImg(img, newWidth) {
+function thumbnailImg(img, newWidth, bLink) {
     var newHeight, thumb;
     if (img && img.tagName.toLowerCase() === "img") {
         newHeight = Math.round(newWidth/img.width * img.height);
-        thumb = document.createElement('a');
-        thumb.href = img.src;
-        thumb.target = "_blank"
+        if (bLink) {
+            thumb = document.createElement('a');
+            thumb.href = img.src;
+            thumb.target = "_blank"
+            thumb.innerHTML = '<span style="background-color: black; color: white; position: absolute; bottom: 0; right: 0;">clique para ampliar</span>';
+        } else {
+            thumb = document.createElement('span');
+        }
         thumb.style.display = "inline-block";
         thumb.style.position = "relative";
         thumb.style.width = newWidth.toString() + "px";
         thumb.style.height = newHeight.toString() + "px";
         thumb.style.backgroundImage = "url(" + img.src + ")";
         thumb.style.backgroundSize = "100%";
-        thumb.innerHTML = '<span style="background-color: black;color: white;position:absolute;bottom:0;right:0;">clique para ampliar</span>';
         img.parentNode.replaceChild(thumb,img);
     }
 }
-function thumbnailImgsFromElement(elem, width, wLimit) {
+function thumbnailImgsFromElement(elem, width, wLimit, bLink) {
     var imgs = elem.getElementsByTagName("img"),
     i, n = imgs.length;
     for (i = 0; i < n; i += 1) {
         if (imgs[i].width > wLimit) {
-            thumbnailImg(imgs[i], width);
+            thumbnailImg(imgs[i], width, bLink);
         }
     }
 }
-function thumbnailImgsFromClass(className, width, wLimit) {
+function thumbnailImgsFromClass(className, width, wLimit, bLink) {
     var elems = document.getElementsByClassName(className),
     i, n = elems.length;
     for (i = 0; i < n; i += 1) {
-        thumbnailImgsFromElement(elems[i], width, wLimit);
+        thumbnailImgsFromElement(elems[i], width, wLimit, bLink);
     }
 }
