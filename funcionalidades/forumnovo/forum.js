6941d232bc6249bc3659dda1e7afa0337bd5862e
NUTED <nuted@SIDESHOWBOB>
2013-05-15 15:47:06 -0300

    adicionando o novo forum e duas imagens perdidas

---
+++ funcionalidades/forumnovo/forum.js
@@ -0,0 +1,167 @@
+var guto = 0; //a variável guto serve pra que o evento de click do 'body' seja ativado sem influenciar no click do '.bloco'
+
+var edicao = '\
+	<div class="esq">\
+	<div class="imagem"></div>\
+		<ul>\
+			<li><input type="text" id="ed_titulo" value="<?=$titulo?>" /></li>\
+			<li class="mensagens">&nbsp;</li>\
+		</ul>\
+	</div>\
+	<div class="dir">\
+		<ul>\
+			<li><textarea id="ed_mens" style="height:66px; overflow:hidden; width:100%;font-family:Trebuchet MS,Tahoma,Verdana; font-size:13px;"><?=$mensagem?></textarea></li>\
+			<li class="criado_por">&nbsp;</li>\
+			<li><div class="enviar" align="right">\
+			<input type="image" src="../../images/botoes/bt_editar.png" />\
+			<input type="image" src="../../images/botoes/bt_excluir.png" onclick=""/>\
+			</div></li>\
+		</ul>\
+	</div>';
+
+var amostra_topico = '';
+var edicao_fid = 0;
+var edicao_tid = 0;
+
+$(document).ready(function(){
+	var cor_original = $(".bloco").css("border-top-color");
+	var fala_original = document.getElementById("balao").innerHTML;
+	
+	//funções para trocar as falas do ajudante e pintar o bloco selecionado
+	$("body").click(function(){ 
+			if (guto != 1){
+				$(".bloco").css("border-top-color",cor_original);
+				$("#balao").html(fala_original);
+			}
+			guto = 0;
+	});
+
+	$("#voltar").click(function(){
+		history.go(-1);
+	});
+
+	$(".bloco").click(function(){
+			switch (this.id){
+				/*case "topicos":
+					$("#balao").html("Tu clicaste no T\&#243;\picos =D");
+				break;*/
+				case "procurar_topico":
+					$("#balao").html("Aqui você busca por discussões anteriores, é só clicar em “Procurar Tópico” e digitar o que procura (não se esqueça de selecionar se quer procurar por “Título”, “Nome” ou “Conteúdo”), após clique em “Procurar”.");
+					$("#resultado_pesquisa").css("border-top-color",cor_original);
+				break;
+				case "resultado_pesquisa":
+					$("#balao").html("Nos resultados da pesquisa, você pode ver tópicos relacionados com as palavras que foram preenchidas na busca.");
+					$("#procurar_topico").css("border-top-color",cor_original);
+				break;
+				case "criar_topico":
+					$("#balao").html("Criação de tópicos para discussões com os seus amigos, colegas e professores.");
+				break;
+				case "bloco_mensagens":
+//					$("#balao").html("Tu clicaste no Mensagens =D");
+					$("#nova_mensagem").css("border-top-color",cor_original);
+				break;
+				case "nova_mensagem":
+//					$("#balao").html("Tu clicaste no Nova Mensagem =D");
+					$("#bloco_mensagens").css("border-top-color",cor_original);
+				break;
+			}
+			$(this).css("border-top-color","#00A99D");
+			guto = 1;
+	});
+	
+	$('#responder_topico').click(function(){
+		$('#nova_mensagem').css('display','block');
+	});
+	$('#cancela_msg').click(function(){
+		escondeNMsg();
+	});
+	$('#envia_msg').click(function(){
+		enviaMens();
+		escondeNMsg();
+	});
+});
+
+var http = false;
+var http_pesq = false;
+
+if (navigator.appName == "Microsoft Internet Explorer"){
+	http_pesq = new ActiveXObject("Microsoft.XMLhttp_mens");
+	http = new ActiveXObject("Microsoft.XMLHTTP");
+}else{
+	http_pesq = new XMLHttpRequest();
+	http = new XMLHttpRequest();
+}
+
+
+function excluir(fid,tid,dtipo){
+	if (confirm("Tem certeza que deseja deletar este tópico? Essa ação não pode ser desfeita.")){
+		var parametros = "fid=" + fid;
+		parametros = parametros + "&topico=" + tid;
+		parametros = parametros + "&deltipo=" + dtipo;
+		parametros = parametros + "&pagina=" + forum_pg;
+	
+		http.abort();
+		http.open("POST", "deltopico.php", true);
+		http.onreadystatechange=function() {
+			if ((http.readyState == 4)&& (http.status == 200 )) {
+
+				document.getElementById("dinamica").innerHTML = http.responseText;
+			}
+		}
+		http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		http.setRequestHeader("Content-length", parametros.length);
+		http.setRequestHeader('Content-Type', "application/x-www-form-urlencoded; charset=utf-8");
+		http.send(parametros);
+	}
+}
+
+function editar(fid,tid){
+	document.location = "forum_cria_topico.php?turma="+fid+"&tid="+tid;
+}
+
+function colore(elemento){
+	quantidade = document.getElementById(elemento).getElementsByTagName('span').length -1;
+	var cor = new Array();
+	cor[0] = '#EEF5F5';
+	cor[1] = '#ccecf4';
+	for (i=0; i < quantidade; i++){
+		document.getElementById('topicos').getElementsByTagName('span')[i].getElementsByTagName('div')[0].style.backgroundColor = cor[i%2];
+	}
+}
+
+function pesquisar(pg, novaPesq){
+	var aux_consulta = document.getElementById('consulta').value;
+	for (i=0;i<document.radio.tipo.length;i++){
+		if (document.radio.tipo[i].checked){
+			aux_tipo = document.radio.tipo[i].value;
+			break;
+		}
+	}
+
+	var parametros = "pagina=" + encodeURI(pg);
+	
+	if (novaPesq){
+		parametros = parametros + "&consulta=" + encodeURI(aux_consulta);
+		parametros = parametros + "&tipo=" + encodeURI(aux_tipo);
+	}
+	
+	http_pesq.abort();
+	http_pesq.open("POST", "pesquisa_forum.php", true);
+	http_pesq.onreadystatechange=function() {
+		if ((http_pesq.readyState == 4)&& (http_pesq.status == 200 )) {
+			document.getElementById("dinamica").innerHTML = http_pesq.responseText;
+		}
+	}
+	http_pesq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http_pesq.setRequestHeader("Content-length", parametros.length);
+	http_pesq.setRequestHeader('Content-Type', "application/x-www-form-urlencoded; charset=utf-8");
+	http_pesq.send(parametros);
+
+}
+
+function ordernar(elemento){
+	var selecao = elemento.options[elemento.selectedIndex].value;
+	if (selecao != ' -- Ordenar... --'){
+		location.href = (location.href+'&ordem='+elemento.selectedIndex);
+	}
+}
---
+++ funcionalidades/forumnovo/forum.php
@@ -0,0 +1,162 @@
+<?php
+	require("../../cfg.php");
+	require("../../bd.php");
+	require("../../funcoes_aux.php");
+
+	//require("../../usuarios.class.php");
+	
+	session_start();
+	
+	$turma = isset($_GET['turma']) ? $_GET['turma'] : 0;
+	
+	require("verifica_user.php");
+	require("sistema_forum.php");
+	require("visualizacao_forum.php");
+	require("../../reguaNavegacao.class.php");
+	
+	$pagina = (isset($_GET['pagina']))? $_GET['pagina'] : 1;
+	
+	if ($VERIFICA_USER_ERRO_ID == 0) {
+		global $tabela_forum; global $tabela_usuarios;
+		
+		$FORUM = new forum($FORUM_ID);
+		$FORUM->configBD($tabela_forum,$tabela_usuarios);
+		$FORUM->topicos($pagina);
+		
+		$paginas = array();
+		$paginas = $FORUM->paginas($pagina,10);
+	}
+	
+	$permissoes = checa_permissoes(TIPOFORUM, $FORUM_ID);
+	if($permissoes === false){
+		die("Funcionalidade desabilitada para a sua turma. Favor voltar.");
+	}
+	
+	$user = new Usuario();
+	$user->openUsuario($_SESSION['SS_usuario_id']);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Planeta ROODA 2.0</title>
+<link type="text/css" rel="stylesheet" href="../../planeta.css" />
+<link type="text/css" rel="stylesheet" href="forum.css" />
+<script type="text/javascript" src="../../jquery.js"></script>
+<script type="text/javascript" src="../../jquery-ui-1.8.1.custom.min.js"></script>
+<script type="text/javascript" src="../../planeta.js"></script>
+<script type="text/javascript" src="../lightbox.js"></script>
+<script type="text/javascript">
+	var forum_pg = <?php echo $pagina; ?>;
+	var deltipo = 0;
+</script>
+<script type="text/javascript" src="forum.js"></script>
+<!--[if IE 6]>
+<script type="text/javascript" src="planeta_ie6.js"></script>
+<![endif]-->
+
+</head>
+
+<body onload="atualiza('ajusta()');inicia();">
+
+<div id="topo">
+	<div id="centraliza_topo">
+		<?php 
+			$regua = new reguaNavegacao();
+			$regua->adicionarNivel("Fórum");
+			$regua->imprimir();
+		?>
+		<p id="bt_ajuda" onmousedown="animacao('abrirAjuda()');">OCULTAR AJUDANTE</p>
+	</div>
+</div>
+
+<div id="geral">
+
+<!-- **************************
+			cabecalho
+***************************** -->
+<div id="cabecalho">
+	<div id="ajuda">
+		<div id="ajuda_meio">
+			<div id="ajudante">
+				<div id="personagem"><img src="../../images/desenhos/ajudante.png" height=145 align="left" alt="Ajudante" /></div>
+				<div id="rel"><p id="balao">Espaço destinado à discussão de diferentes
+				temáticas com usuários contribuindo em tempos e espaços diferentes.</p></div>
+			</div>
+		</div>
+		<div id="ajuda_base"></div>
+	</div>
+</div><!-- fim do cabecalho -->
+	
+	
+<div id="conteudo_topo"></div><!-- para a imagem de fundo do topo -->
+<div id="conteudo_meio"><!-- para a imagem de fundo do meio -->
+
+<!-- **************************
+			conteudo
+***************************** -->
+	<div id="conteudo"><!-- tem que estar dentro da div 'conteudo_meio' -->
+	
+	<div class="bts_cima">
+<?php
+$linkcria = "forum_cria_topico.php?fid=$FORUM_ID&amp;turma=$turma";
+if ($user->podeAcessar($permissoes['forum_criarTopico'], $FORUM_ID)) echo '<input align="left" type="image" src="../../images/botoes/bt_criar_topico.png" onclick="document.location = \''.$linkcria.'\';"/>';
+?>
+	<input align="right" type="image" src="../../images/botoes/bt_procurar_topico.png" onclick="document.location='forum_procurar.php?turma=<?=$turma?>'"/>
+	</div>
+	
+	<div id="dinamica">
+<?php
+		if ($VERIFICA_USER_ERRO_ID == 0) {
+			if ($FORUM->contador > 0){
+				mostraPaginas ($paginas, $pagina, false, "forum.php?turma=$FORUM_ID");
+?>
+
+	<div id="topicos" class="bloco">
+		<h1>TÓPICOS</h1> <?php
+				$forum_msg_cont = count($FORUM->mensagem);
+				for ($i=0; $i<$forum_msg_cont; $i++){
+					$topico = $FORUM->mensagem[$i];
+					mostraTopicos($topico->msgId,$topico->msgUserName,$topico->msgTitulo,$topico->msgTexto,$topico->msgData,$topico->msgQntFilhos,($i % 2), $topico->msgUserId);
+				}
+?>
+	</div><!-- fim da div topicos -->
+<?php			
+				mostraPaginas ($paginas, $pagina, false, "forum.php?turma=$FORUM_ID");
+
+			}else{
+?>
+	<div id="topicos" class="bloco">
+		<h1>TÓPICOS</h1><?php mostraAviso(5);?>
+	</div><!-- fim da div topicos -->
+<?php
+			}
+		}else{
+?>
+	<div id="topicos" class="bloco">
+		<h1>TÓPICOS</h1><?php mostraAviso($VERIFICA_USER_ERRO_ID);?>
+	</div><!-- fim da div topicos -->
+<?php
+		}
+?>
+	
+	</div>
+	
+	<div class="bts_baixo">
+<?php
+$linkcria = "forum_cria_topico.php?turma=$FORUM_ID";
+if ($user->podeAcessar($permissoes['forum_criarTopico'], $FORUM_ID)) echo '<input align="left" type="image" src="../../images/botoes/bt_criar_topico.png" onclick="document.location = \''.$linkcria.'\';"/>'; ?>
+	<input align="right" type="image" src="../../images/botoes/bt_procurar_topico.png" onclick="document.location='forum_procurar.php?turma=<?=$turma?>'"/>
+	</div>
+	
+	</div>
+
+	<!-- fim do conteudo -->
+
+</div>
+<div id="conteudo_base"></div><!-- para a imagem de fundo da base -->
+
+
+</div><!-- fim da geral -->
+</body>
+</html>
