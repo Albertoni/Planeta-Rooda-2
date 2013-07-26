337e66b4f50f96e4277cef684c31ac425df8ae48
Alexandre Leuck <aleuck2004@gmail.com>
2013-07-26 18:50:22 -0300

    some progress

--- arquivo.class.php
+++ arquivo.class.php
@@ -1,7 +1,8 @@
 <?php //>
 require_once("cfg.php");
 require_once("bd.php");
-class Arquivo {
+class Arquivo
+{
 	private $id;
 	private $tipoFuncionalidade = 0; // deve ser removido (fazer 1 tabela de referencia para cada funcionalidade em favor da modularidade)
 	private $idFuncionalidade = 0;   // deve ser removido (fazer 1 tabela de referencia para cada funcionalidade em favor da modularidade)
@@ -18,12 +19,16 @@ class Arquivo {
 	private $upload = false;
 	private $download = false;
 
-	public function Arquivo($id = false) {
+	public function __construct($id = false)
+	{
 		global $tabela_arquivos;
 		$id = (int) $id;
-		if ($id === false) {
+		if ($id === false)
+		{
 			$upload = true;
-		} else {
+		}
+		else
+		{
 			$this->id = $id;
 			$bd = new conexao();
 			$bd->solicitar(
@@ -41,20 +46,25 @@ class Arquivo {
 				FROM $tabela_arquivos
 				WHERE arquivo_id = '$id'"
 			);
-			if ($bd->erro) {
+			if ($bd->erro)
+			{
 				// erro na consulta;
 				$this->erros[] = "mysql: " . $bd->erro;
 			}
-			if ($bd->registros === 1) {
+			if ($bd->registros === 1)
+			{
 				// carrega arquivo encontrado
 				$this->popular($bd->resultado);
 				$download = true;
-			} else {
+			}
+			else
+			{
 				$this->erros[] = "Arquivo n&atilde;o encontrado";
 			}
 		}
 	}
-	private function popular($resultadoBd) {
+	private function popular($resultadoBd)
+	{
 		$this->conteudo = $resultadoBd['conteudo']; // conteudo do arquivo
 		$this->titulo = $resultadoBd['titulo'];     // titulo do arquivo
 		$this->nome = $resultadoBd['nome'];
@@ -66,57 +76,74 @@ class Arquivo {
 		$this->idFuncionalidade = $resultadoBd['idFuncionalidade'];
 		$this->idUploader = $resultadoBd['idUploader'];
 	}
-	public function getId() {
+	public function getId()
+	{
 		return $this->id;
 	}
-	public function getConteudo() {
+	public function getConteudo()
+	{
 		return $this->conteudo;
 	}
-	public function getTitulo() {
+	public function getTitulo()
+	{
 		return $this->titulo;
 	}
-	public function getNome() {
+	public function getNome()
+	{
 		return $this->nome;
 	}
-	public function getTipo() {
+	public function getTipo()
+	{
 		return $this->tipo;
 	}
-	public function getTamanho() {
+	public function getTamanho()
+	{
 		return $this->tamanho;
 	}
-	public function getTags() {
+	public function getTags()
+	{
 		$tags = array();
-		foreach ($this->tags as $value) {
+		foreach ($this->tags as $value)
+		{
 			$tags[] = $value;
 		}
 		return $tags;
 	}
-	public function getErros() {
+	public function getErros()
+	{
 		$erros = array();
-		foreach ($this->erros as $value) {
+		foreach ($this->erros as $value)
+		{
 			$erros[] = $value;
 		}
 		return $erros;
 	}
-	public function temErros() {
+	public function temErros()
+	{
 		return (0 !== count($this->erros));
 	}
-	public function getData() {
+	public function getData()
+	{
 		return $this->data;
 	}
-	public function getTipoFuncionalidade() {
+	public function getTipoFuncionalidade()
+	{
 		return $this->tipoFuncionalidade;
 	}
-	public function getIdFuncionalidade() {
+	public function getIdFuncionalidade()
+	{
 		return $this->idFuncionalidade;
 	}
-	public function getIdUploader() {
+	public function getIdUploader()
+	{
 		return $this->idUploader;
 	}
 	// METODOS RELACIONSADOS A UPLOAD
-	public function salvar() {
+	public function salvar()
+	{
 		// NOVO ARQUIVO
-		if ($this->upload && !$this->download) {
+		if ($this->upload && !$this->download)
+		{
 			// novo arquivo
 			$bd = new conexao();
 			// sanitizando dados para o banco de dados
@@ -145,16 +172,21 @@ class Arquivo {
 				"INSERT INTO $tabela_arquivos (" . implode(", ", $campos) . ")
 				VALUES ('" . implode("', '", $valores) . "')"
 			);
-			if ($bd->erro !== "") {
+			if ($bd->erro !== "")
+			{
 				$this->erros[] = "BD: {$bd->erro}";
-			} else {
+			}
+			else
+			{
 				$this->id = $bd->ultimo_id();
 				$this->upload = false;
 				$this->download = true;
 				return true;
 			}
 		// MUDANDO ARQUIVO ANTIGO
-		} else if ($this->download && !$this->upload) {
+		}
+		else if ($this->download && !$this->upload)
+		{
 			$bd = new conexao();
 			// sanitizando dados para o banco de dados
 			$campos[]  = 'titulo';
@@ -179,7 +211,8 @@ class Arquivo {
 			$valores[] = (int) $this->idUploader;
 			$sqlset = array();
 			// construindo a sintaxe do sql
-			foreach ($campos as $num => $campo) {
+			foreach ($campos as $num => $campo)
+			{
 				$sqlset[] = "{$campo} = '{$valores[$num]}'";
 			}
 			// executando consulta
@@ -188,12 +221,15 @@ class Arquivo {
 				SET " . implode(", ", $sqlset) . "
 				WHERE arquivo_id = {$this->id}"
 			);
-			if ($bd->erro !== '') {
+			if ($bd->erro !== '')
+			{
 				$this->erros[] = $bd->erro;
 				return false;
 			}
 			return true;
-		} else {
+		}
+		else
+		{
 			$this->erros[] = "Este arquivo não pode ser enviado";
 			return false;
 		}
@@ -203,7 +239,8 @@ class Arquivo {
 	// 	}
 	// }
 	public function setFuncionalidade($tipo, $id) {
-		if ($upload === true) {
+		if ($upload === true)
+		{
 			$tipo = (int) $tipo;
 			$id = (int) $id;
 			$this->tipoFuncionalidade = $tipo;
@@ -213,19 +250,27 @@ class Arquivo {
 	}
 	public function setIdUploader($id) {
 		$id = (int) $id;
-		if ($id === 0) {
+		if ($id === 0)
+		{
 			$this->erros[] = "Id inválido (nulo)";
-		} else {
+		}
+		else
+		{
 			$this->idUploader = $id;
 		}
 		return $this;
 	}
 	public function setArquivo($FILE) {
-		if (!isset($FILE['tmp_name']) || !$FILE['tmp_name']) {
+		if (!isset($FILE['tmp_name']) || !$FILE['tmp_name'])
+		{
 			$this->erros[] = "Parametro inv&aacute;lido (Arquivo::setArquivo($FILE))";
-		} if (!filesize($FILE['tmp_name'])) {
+		}
+		if(!filesize($FILE['tmp_name']))
+		{
 			$this->erros[] = "Arquivo vazio ou inválido.";
-		} else {
+		}
+		else
+		{
 			$this->tamanho = $FILE['size'];
 			$arquivo = fopen($FILE['tmp_name'], 'r');
 			$this->conteudo = fread($arquivo, filesize($FILE['tmp_name']));
@@ -235,44 +280,55 @@ class Arquivo {
 		}
 		return $this;
 	}
-	public function setConteudo($conteudo) {
+	public function setConteudo($conteudo)
+	{
 		$this->conteudo = $conteudo;
 	}
-	public function setTitulo($titulo) {
+	public function setTitulo($titulo)
+	{
 		$titulo = trim($titulo);
 		$this->titulo = $titulo;
 		return $this;
 	}
-	public function setNome($nome) {
+	public function setNome($nome)
+	{
 		$nome = trim($nome);
 		$this->nome = $nome;
 		return $this;
 	}
-	public function setTipo($tipo) {
+	public function setTipo($tipo)
+	{
 		$tipo = trim($tipo);
 		$this->tipo = $tipo;
 		return $this;
 	}
-	public function setTags($tags) {
-		if (is_string($tags)) {
+	public function setTags($tags)
+	{
+		if (is_string($tags))
+		{
 			$tags = explode(",", $tags);
 		}
 		// nada de 'else' aqui.
-		if (is_array($tags)) {
+		if (is_array($tags))
+		{
 			$this->tags = array();
-			foreach ($tags as $value) {
+			foreach ($tags as $value)
+			{
 				$this->tags[] = trim($value);
 			}
 		}
 		return $this;
 	}
-	public function excluir() {
-		if (!$this->upload && $this->download) {
+	public function excluir()
+	{
+		if (!$this->upload && $this->download)
+		{
 			$bd = new conexao();
 			$bd->solicitar(
 				"DELETE FROM $tabela_arquivos WHERE arquivo_id = {$this->id}"
 			);
-			if ($bd->erro !== '') {
+			if ($bd->erro !== '')
+			{
 				$this->erros[] = "DB:" . $bd->erro;
 				return false;
 			}
--- uploadFile2.php
+++ uploadFile2.php
@@ -5,31 +5,33 @@ require_once("cfg.php");
 require_once("bd.php");
 require_once("arquivo.class.php");
 
-$idUsuario = isset($_SESSION['SS_usuario_id']) ? $_SESSION['SS_usuario_id'] : 0;
+$idUsuario   = isset($_SESSION['SS_usuario_id'])   ? $_SESSION['SS_usuario_id']   : 0;
 $nomeUsuario = isset($_SESSION['SS_usuario_nome']) ? $_SESSION['SS_usuario_nome'] : "";
 
-$idFuncionalidade = isset($_GET['funcionalidade_id']) ? (int) $_GET['funcionalidade_id'] : 0;
+$idFuncionalidade   = isset($_GET['funcionalidade_id'])   ? (int) $_GET['funcionalidade_id']   : 0;
 $tipoFuncionalidade = isset($_GET['funcionalidade_tipo']) ? (int) $_GET['funcionalidade_tipo'] : 0;
 
 $jsonArray = array();
-if ($idUsuario <= 0){
+if ($idUsuario <= 0)
+{
 	$json['errors'][] = "Você não está mais logado. Por favor, autentique-se novamente.";
-} else {
+}
+else
+{
 	if ($idFuncionalidade <= 0 || $tipoFuncionalidade <= 0)
 	{
 		$json['errors'][] = "Parametros invalidos"; // Sabe SQL injection?
 	}
 	else
 	{
-
 		if(isset($_POST['upload']) && isset($_FILES['userfile']) && $_FILES['userfile']['size'] > 0)
 		{
 			$fileName = $_FILES['userfile']['name'];
 			$tmpName  = $_FILES['userfile']['tmp_name'];
 			$fileSize = (int) $_FILES['userfile']['size'];
 			$fileType = $_FILES['userfile']['type'];
-			$titulo = isset($_POST['arquivo_titulo']) ? $_POST['arquivo_titulo'] : false;
-			$autor = isset($_POST['arquivo_autor']) ? $_POST['arquivo_autor'] : false;
+			$titulo   = isset($_POST['arquivo_titulo']) ? $_POST['arquivo_titulo'] : false;
+			$autor    = isset($_POST['arquivo_autor'])  ? $_POST['arquivo_autor']  : false;
 
 			$arquivo = new Arquivo();
 			$arquivo->setArquivo($_FILES['userfile']);
@@ -40,9 +42,12 @@ if ($idUsuario <= 0){
 			if ($titulo) $arquivo->setTitulo($titulo);
 			if ($autor) $arquivo->setAutor($autor);
 			
-			if ($arquivo->temErros()) {
+			if ($arquivo->temErros())
+			{
 				$json['erros'] = $arquivo->getErros();
-			} else {
+			}
+			else
+			{
 				$arquivo->salvar();
 			}
 		}
