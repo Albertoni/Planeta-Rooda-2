a93e5128667b48aae9005093d0c9d90c056cf7f9
Alexandre Leuck <aleuck2004@gmail.com>
2013-11-06 21:35:53 -0200

    usar sempre int para tamanho de arquivo

--- arquivo.class.php
+++ arquivo.class.php
@@ -135,7 +135,7 @@ class Arquivo {
 			$campos[]  = 'tipo';
 			$valores[] = $bd->sanitizaString($this->tipo);
 			$campos[]  = 'tamanho';
-			$valores[] = $bd->sanitizaString($this->tamanho);
+			$valores[] = (int) $this->tamanho);
 			$campos[]  = 'arquivo';
 			$valores[] = $bd->sanitizaString($this->conteudo);
 			$campos[]  = 'md5';
@@ -182,7 +182,7 @@ class Arquivo {
 			$campos[]  = 'tipo';
 			$valores[] = $bd->sanitizaString($this->tipo);
 			$campos[]  = 'tamanho';
-			$valores[] = $bd->sanitizaString($this->tamanho);
+			$valores[] = (int) $this->tamanho;
 			$campos[]  = 'arquivo';
 			$valores[] = $bd->sanitizaString($this->conteudo);
 			$campos[]  = 'md5';
