a1ae88f65828d58bccf2203e3cd248a48379ab8a
NUTED <nuted@SIDESHOWBOB>
2013-11-18 18:12:52 -0200

    prototipo de adicionador de arquivos

---
+++ APAGAR_ISSO_DEPOIS_DO_CURSO.php
@@ -0,0 +1,63 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+require_once("cfg.php");
+require_once("bd.php");
+require_once("funcoes_aux.php");
+session_start();
+if (!isset($_SESSION['SS_usuario_id'])){ // Se isso não estiver setado, o usuario não está logado
+	die("<a href=\"index.php\">Por favor volte e entre em sua conta.</a>");
+}
+
+/* APAGAR ISSO DEPOIS DO CURSO */
+
+if (isset($_POST['codTurma']) and isset($_POST['codAluno']) and isset($_POST['nivel'])){
+	// tudo numerico, só pegar os ints
+	$codTurma = (int) $_POST['codTurma'];
+	$codAluno = (int) $_POST['codAluno'];
+	$nivel = (int) $_POST['nivel'];
+	
+	$q = new conexao();
+	$q->solicitar("INSERT INTO TurmasUsuario VALUES ($codTurma, $codAluno, $nivel)");
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<form method="post">
+<ul>
+	<li>Turma:
+		<select name="codTurma">
+<?php
+$listaTurmas = new conexao();
+$listaTurmas->solicitar("SELECT codTurma, nomeTurma FROM Turmas ORDER BY codTurma DESC");
+
+for ($i=0;$i<$listaTurmas->registros;$i++){
+	echo "<option value=\"".$listaTurmas->resultado['codTurma']."\">".$listaTurmas->resultado['nomeTurma']."</option>\n";
+	$listaTurmas->proximo();
+}
+?>
+		</select>
+	<li>Aluno:
+		<select name="codAluno">
+
+<?php
+$listaAlunos = new conexao();
+$listaAlunos->solicitar("SELECT usuario_id, usuario_nome FROM usuarios ORDER BY usuario_id DESC LIMIT 100");
+
+for($i=0; $i < $listaAlunos->registros; $i++){
+	echo "<option value=\"".$listaAlunos->resultado['usuario_id']."\">".$listaAlunos->resultado['usuario_nome']."</option>\n";
+	$listaAlunos->proximo();
+}
+?>
+		</select>
+	<li>Nivel: <select name="nivel">
+			<option value="4">Professor</option>
+			<option value="8">Monitor</option>
+			<option value="16">Aluno</option>
+		</select>
+	<li><input type="submit" value="Adiciona!">
+</ul>
+</form>
+</body>
+</html>
