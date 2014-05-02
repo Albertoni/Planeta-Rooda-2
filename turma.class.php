a026cbbd3a9b6687b12e159637ad1783bb6d7384
João Albertoni <nuted@SIDESHOWBOB>
2014-05-02 18:42:06 -0300

    orientação a objetos da turma refeita: código anterior tinha muitas partes não utilizadas e era dificil de modificar

--- turma.class.php
+++ turma.class.php
@@ -12,14 +12,24 @@ class Turma{
 	private $idPlaneta;
 	private $NivelPermissao = array(); //Array que conterá o nível de permissao dos usuarios de uma turma, indexado por CodUsuario.
 	
-	function getId(){	return id;}
-	function getNomeTurma(){	return nomeTurma;}
-	function getProfResponsavel(){	return profResponsavel;}
-	function getDescricao(){	return descricao;}
-	function getSerie(){	return serie;}
-	function getEscola(){	return escola;}
-	function getIdChat(){	return idChat;}
-	function getIdPlaneta(){	return idPlaneta;}
+	const BLOG = 1;
+	const PORTFOLIO = 2;
+	const BIBLIOTECA = 3;
+	const PERGUNTA = 4;
+	const AULAS = 5;
+	const COMUNICADOR = 6;
+	const FORUM = 7;
+	const ARTE = 8;
+	const PLAYER = 10;
+
+	function getId()				{return $this->id;}
+	function getNome()				{return $this->nomeTurma;}
+	function getProfResponsavel()	{return $this->profResponsavel;}
+	function getDescricao()			{return $this->descricao;}
+	function getSerie()				{return $this->serie;}
+	function getEscola()			{return $this->escola;}
+	function getIdChat()			{return $this->idChat;}
+	function getIdPlaneta()			{return $this->idPlaneta;}
 	
 	function __construct(	$novoNomeTurma = "",
 							$novoProfResponsavel = 0,
@@ -30,16 +40,16 @@ class Turma{
 							$novoIdPlaneta = 0 ){
 		
 		$this->nomeTurma = $novoNomeTurma;
-		$this->profResponsavel = $novoProfResponsavel;
+		$this->profResponsavel = (int) $novoProfResponsavel;
 		$this->descricao = $novaDescricao;
 		$this->serie = $novaSerie;
 		$this->escola = $novaEscola;
-		$this->idChat = $novoIdChat;
-		$this->idPlaneta = $novoIdPlaneta;
+		$this->idChat = (int) $novoIdChat;
+		$this->idPlaneta = (int) $novoIdPlaneta;
 		//O array nivel de permissao sera inicializado ao abrir uma turma.
 	}
 	
-	function abrir($id){
+	function openTurma($id){
 		$q = new conexao();
 		$idSanitizado = $q->sanitizaString($id);
 
@@ -52,10 +62,10 @@ class Turma{
 				$q->resultado['descricao'],
 				$q->resultado['serie'],
 				$q->resultado['Escola'],
-				$q->resultado['chat_ID'],
-				$q->resultado['idPlaneta'],
+				$q->resultado['chat_id'],
+				$q->resultado['idPlaneta']
 				);
-			$this->id = $q->resultado['codTurma'];
+			$this->id = (int) $q->resultado['codTurma'];
 			$this->salvo = true;
 			//consultado no DB pelos usuarios da turma e..
 			$q->solicitar("SELECT * FROM TurmasUsuario WHERE codTurma = '$id'");
@@ -129,5 +139,102 @@ class Turma{
 		$json['idPlaneta']  = $this->idPlaneta;
 
 		return json_encode($json);
+	}
+	/*
+		TODO:
+	
+	* Determina se houve alterações na funcionalidade desta turma.
+	* @param funcionalidade_param Funcionalidade a ser verificada.
+	* @param data_param Data à partir da qual uma alteração deve ter acontecido para ser retornada.
+	* @return Número de alterações que ocorreram na dada funcionalidade desta turma desde a data passada.
+	*/
+	public function getNumeroAlteracoes($funcionalidade_param, $data_param){
+		$alteracoes = 0;
+		$sql = '';
+		
+		// DEBUG REMOVER ISSO ASSIM QUE O TEMPO DE ULTIMO LOGIN FOR IMPLEMENTADO
+		$data_param = strtotime("-5 years"); //código de teste para mostrar para as gurias na reunião...
+		$data_param = date("Y-m-d H:i:s", $data_param);
+		
+		switch($funcionalidade_param){
+			case turma::BIBLIOTECA: $sql = "SELECT *
+											FROM (
+													SELECT COUNT(*) AS alteracoes
+													FROM BibliotecaMateriais
+													WHERE codTurma = ".$this->id." AND '$data_param'<=data
+											) AS BMalteracoes
+											UNION ALL(
+												SELECT COUNT(*) AS alteracoes
+												FROM BibliotecaComentarios AS BC JOIN BibliotecaMateriais AS BM ON BC.codMaterial = BM.codMaterial
+												WHERE BM.codTurma = ".$this->id." AND '$data_param'<=BC.data
+											)";
+				break;
+			case turma::BLOG:		$sql = "SELECT *
+											FROM (
+													SELECT COUNT(*) AS alteracoes
+													FROM blogblogs AS B JOIN usuarios AS U ON OwnersIds = usuario_id
+																		JOIN TurmasUsuario AS TU ON TU.codUsuario = U.usuario_id
+																		JOIN blogposts AS BP ON BP.BlogId = B.Id
+													WHERE B.Tipo=1 AND TU.codTurma=".$this->id." AND '$data_param'<=BP.Date
+											) AS BPalteracoes
+											UNION ALL(
+												SELECT COUNT(*) AS alteracoes
+												FROM blogblogs AS B JOIN usuarios AS U ON OwnersIds = usuario_id
+																	JOIN TurmasUsuario AS TU ON TU.codUsuario = U.usuario_id
+																	JOIN blogposts AS BP ON BP.BlogId = B.Id
+																	JOIN blogcomentarios AS BC ON BC.PostId = BP.Id
+												WHERE B.Tipo=1 AND TU.codTurma=".$this->id." AND '$data_param'<=BC.Date
+											)";
+				break;
+			case turma::FORUM:		$sql = "";
+				break;
+			case turma::ARTE:		$sql = "SELECT *
+											FROM (
+													SELECT COUNT(*) AS alteracoes
+													FROM ArtesDesenhos
+													WHERE codTurma=".$this->id." AND '$data_param'<=Data
+											) AS ADalteracoes
+											UNION ALL(
+												SELECT COUNT(*) AS alteracoes
+												FROM ArtesComentarios AS AC JOIN ArtesDesenhos AS AD ON AC.CodDesenho=AD.CodDesenho
+												WHERE codTurma=".$this->id." AND '$data_param'<=AC.Data
+											)";
+				break;
+			case turma::PERGUNTA:	$sql = "SELECT COUNT(*) AS alteracoes
+											FROM PerguntaQuestionarios
+											WHERE turma = ".$this->id." AND '$data_param'<=datainicio";
+				break;
+			case turma::PORTFOLIO:	$sql = "SELECT *
+											FROM (
+													SELECT COUNT(*) AS alteracoes
+													FROM PortfolioProjetos 
+													WHERE turma=".$this->id." AND '$data_param'<=dataCriacao
+											) AS PPalteracoes
+											UNION ALL(
+												SELECT COUNT(*) AS alteracoes
+												FROM PortfolioProjetos AS PP JOIN PortfolioPosts AS PPo ON PP.id=PPo.projeto_id
+												WHERE PP.turma=".$this->id." AND ('$data_param'<=PPo.dataCriacao OR '$data_param'<=PPo.dataUltMod)
+											)";
+				break;
+			case turma::PLAYER:		$sql = "SELECT COUNT(*)
+											FROM PlayerComentarios AS PC JOIN PlayerVideos AS PV ON PC.id_video=PV.id
+											WHERE PV.turma=".$this->id." AND '$data_param'<=PC.data";
+				break;
+			case turma::AULAS:		$sql = "";
+				break;
+		}
+		if($sql != ''){
+			$conexaoAlteracoes = new conexao();
+			$conexaoAlteracoes->solicitar($sql);
+			if(isset($conexaoAlteracoes->resultado['alteracoes'])){
+				$alteracoes = 0;
+				for($i=0; $i<$conexaoAlteracoes->registros; $i++){
+					$alteracoes += $conexaoAlteracoes->resultado['alteracoes'];
+					$conexaoAlteracoes->proximo();
+				}
+			}
+			
+		}
+		return $alteracoes;
 	}
 }
