e202df6604b6b3ac2769f9d7df1f44bc5ed9b252
João Albertoni <nuted@SIDESHOWBOB>
2014-05-05 16:06:16 -0300

    geração de nova senha passada parcialmente para orientação a objetos, configurada para funcionar com o código atual

--- novasenha.php
+++ novasenha.php
@@ -4,31 +4,36 @@
 	require_once("funcoes_aux.php");
 
 	$data="";
-	$email = $_POST['email'];			  
-	$email = addslashes($email);
+	$pesquisa1 = new conexao();
+	$email = $_POST['email'];
+	$email = $pesquisa1->sanitizaString($email);
 
-	$pesquisa1 = new conexao($BD_host1,$BD_base1,$BD_user1,$BD_pass1);
+	
 	if($pesquisa1->erro != "") 
 	{ 
 		$data .= '{ "erro":"1", "texto":"Houve um erro no banco de dados"}';
 	}else{
 		$pesquisa1->solicitar("select * from $tabela_usuarios WHERE usuario_email = '$email' LIMIT 1");
-		if	(($pesquisa1->erro!= "")||($pesquisa1->registros != 1))
-		{ 
+		if	(($pesquisa1->erro!= "")||($pesquisa1->registros != 1)){
 			$data .= '{ "erro":"1", "texto":"Houve um erro no banco de dados"}';
 		}else{
-			$usuario = $pesquisa1->resultado['usuario_login'];
+			$usuarioLogin = $pesquisa1->resultado['usuario_login'];
 			$usuario_id = $pesquisa1->resultado['usuario_id'];
 			$length = 8;
 			$passwordrand = '';
 			$pattern = "1234567890abcdefghijklmnopqrstuvwxyz";
+			$tamanhoPattern = (strlen($pattern) - 1); // -1 porque começa em 1 e arrays começam em 0
+
 			for($i=0;$i<$length;$i++){
-				$passwordrand .= $pattern{rand(0,35)};
+				$passwordrand .= $pattern{rand(0,$tamanhoPattern)};
 			}
-			$passwordrand2 = md5($passwordrand);
+			$usuario = new Usuario();
+			$usuario->openUsuario($usuario_id);
+			$usuario->setPassword($passwordrand);
+			//$passwordrand2 = crypt($passwordrand, "$2y$07$".gen_salt(22));
 
 			$assunto = "Nova senha para o Planeta 2";
-			$mensagem = "Foi solicitada uma nova senha para o usuário '$usuario' no Planeta 2.\n";
+			$mensagem = "Foi solicitada uma nova senha para o usuário '$usuarioLogin' no Planeta 2.\n";
 			$mensagem .= "Nova senha: $passwordrand\n\n";
 			$mensagem .= "Para continuar com a senha antiga, basta ignorar este email.\n";
 			
@@ -38,10 +43,7 @@
 				$data .= '{ "erro":"1", "texto":"Houve um erro no envio da senha para seu email"}';
 			}else{
 				$data .= '{ "erro":"0", "texto":"Sua nova senha foi enviada para seu email"}';
-				$pesquisa1->solicitar("update $tabela_usuarios set usuario_nova_senha='$passwordrand2' where usuario_id = '$usuario_id'");
 			}
 		}
 	}
-	echo '{"mensagem":'.$data.'}';	
-
-?>
+	echo '{"mensagem":'.$data.'}';
