28234e630f74217fcb81f59a26cb2c77ce047454
Guilherme <gmottin27@gmail.com>
2014-05-06 15:21:36 -0300

    Implementada a criacao do planeta ao salvar uma turma e sobreposicao do arquivo Planeta-Guilherme sobre planeta para garantir que a classe usada seja a implementação mais recente.

--- planeta.class.php
+++ planeta.class.php
@@ -1,75 +1,103 @@
 <?php
-	
-require_once("funcoes_aux.php");
-require_once("cfg.php");
-require_once("bd.php");
-require_once("usuarios.class.php");
-
 class Planeta{
-//dados
-	/*
-	* Id do planeta no banco de dados.
-	*/
 	private $id;
-	
-	/*
-	* Nome do planeta no banco de dados.
-	*/
 	private $nome;
-	
-	/*
-	* Id no banco de dados do principal terreno do planeta.
-	*/
-	private $idTerrenoPrincipal;
-
-	/*
-	* Aparência do planeta.
-	*/
 	private $aparencia;
-	const PLANETA_VERDE='1';
-	const PLANETA_GRAMA='2';
-	const PLANETA_LAVA='3';
-	const PLANETA_GELO='4';
-	const PLANETA_URBANO='5';
-	const PLANETA_QUARTO='6';
-	
-//métodos
-	function Planeta(){
-		
+	private $ehVisitante;
+	private $idTerrenoPrincipal;
+	private $idTerrenoPatio;
+
+	function __construct(	$novoNome = "",
+							$novaAparencia = 0,
+							$novoEhVisitante = 0,
+							$novoIdTerrenoPrincipal = 0,
+							$novoIdTerrenoPatio = 0
+						){
+		$this->nome		= $novoNome;
+		$this->aparencia= $novaAparencia;
+		$this->ehVisitante	= $novoEhVisitante;
+		$this->idTerrenoPrincipal	= $novoIdTerrenoPrincipal;
+		$this->idTerrenoPatio	= $novoIdTerrenoPatio;
 	}
-	
-	/*
-	* Dada uma id, preenche os campos com os dados do planeta encontrados no banco de dados.
-	* @param idPlaneta_param Id do planeta a ser buscada no banco de dados.
-	*/
-	public function openPlaneta($idPlaneta_param){
-		$this->id = (int) $idPlaneta_param;
-		
-		$conexaoDadosPlaneta = new conexao();
-		$conexaoDadosPlaneta->solicitar("SELECT * FROM Planetas WHERE Id=".($this->id));
-		
-		$this->idTerrenoPrincipal = $conexaoDadosPlaneta->resultado['IdTerrenoPrincipal'];
-		$this->idTerrenoPatio = $conexaoDadosPlaneta->resultado['IdTerrenoPatio'];
-		
-		$this->nome = $conexaoDadosPlaneta->resultado['Nome'];
-		$this->aparencia = $conexaoDadosPlaneta->resultado['Aparencia'];
+
+	function getId()	{return $this->id;}
+	function getNome()	{return $this->nome;}
+	function getAparencia()	{return $this->aparencia;}
+	function getEhVisitante(){return $this->ehVisitante;}
+	function getIdTerrenoPrincipal()	{return $this->idTerrenoPrincipal;}
+	function getIdTerrenoPatio()	{return $this->idTerrenoPatio;}
+
+	function abrir($id){
+		$q = new conexao();
+		$idSanitizado = $q->sanitizaString($id);
+
+		$q->solicitar("SELECT * FROM Planetas WHERE Id = '$id'");
+
+		if($q->registros > 0){
+			$this->__construct(
+				$q->resultado['Nome'],
+				$q->resultado['Aparencia'],
+				$q->resultado['EhVisitante'],
+				$q->resultado['IdTerrenoPrincpal'],
+				$q->resultado['IdTerrenoPatio']
+				);
+			$this->id = $q->resultado['Id'];
+			$this->salvo = true;
+		}else{
+			$this->__construct("Planeta inexistente");
+		}
 	}
-	
-	public function getId(){ return $this->id; }
-	public function getNome(){ return $this->nome; }
-	public function getAparencia(){ return $this->aparencia; }
-	public function getIdTerrenoPrincipal(){ return $this->idTerrenoPrincipal; }
-	
-	/*
-	* Retornará a id do icone deste planeta em IconesPlanetas
-	*/
-	public function getIdIcone(){
-		$conexaoImagemIcone = new conexao();
-		$conexaoImagemIcone->solicitar("SELECT * 
-										FROM IconesPlanetas 
-										WHERE planeta=".$this->id);
-		return $conexaoImagemIcone->resultado['id'];
+
+	function salvar(){
+		$q = new conexao();
+		if($this->salvo === false){
+			$nomeSanitizado = $q->sanitizaString($this->nome);
+			$aparenciaSanitizada = (int) $this->aparencia;
+			$ehVisitanteSanitizado = ($this->ehVisitante ? 1 : 0);
+			$idTerrenoPrincipalSanitizado = (int) $this->idTerrenoPrincipal;
+			$idTerrenoPatioSanitizado = (int) $this->idTerrenoPatio;
+			$idTurmaSanitizado = (int) $this->idTurma;
+
+			$q->solicitar("
+				INSERT INTO Planetas
+					(Nome, Aparencia, EhVisitante, IdTerrenoPrincipal, IdTerrenoPatio);
+				VALUES(
+					'$nomeSanitizado',
+					'$aparenciaSanitizada',
+					'$ehVisitanteSanitizado',
+					'$idTerrenoPrincipalSanitizado',
+					'$idTerrenoPatioSanitizado',
+					'$idTurmaSanitizado')");
+
+			if($q->erro == ""){
+				$this->id = $q->ultimoId();
+				$this->salvo = true;
+			}
+		}else{
+			$query = ("
+				UPDATE Planetas SET 
+					Nome   = '$this->nomeSanitizado',
+					Aparencia   = '$this->aparenciaSanitizada',
+					EhVisitante = '$this->ehVisitanteSanitizado',
+					IdTerrenoPrincipal  = '$this->idTerrenoPrincipalSanitizado',
+					IdTerrenoPatio = '$this->idTerrenoPatioSanitizado'
+				WHERE Id = '$this->id'");
+		}
 	}
-	
-	
+
+	function toJson($sendHeaders = false){
+		if($sendHeaders){
+			header("Content-Type: application/json");
+		}
+		
+		$json = [];
+		$json['id']     = $this->id;
+		$json['nome']   = $this->nome;
+		$json['aparencia']   = $this->aparencia;
+		$json['ehVisitante'] = $this->ehVisitante;
+		$json['idTerrenoPrincipal']  = $this->idTerrenoPrincipal;
+		$json['idTerrenoPatio']  = $this->idTerrenoPatio;
+
+		return json_encode($json);
+	}
 }
